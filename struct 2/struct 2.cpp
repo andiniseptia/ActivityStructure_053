4f4bedbc748e23d26ad567b4ee1c110aa10615b5
andiniseptia <andiniseptiandri124@gmail.com>
2023-04-05 11:10:34 +0700

    Structur di dalam structur

---
+++ struct 2/struct 2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+struct DetailAlamat
+{
+	char desa[20];
+	char kota[20];
+};
+
+struct Mahasiswa
+{
+	char nim[12];
+	char nama[20];
+	DetailAlamat alamat;
+	int umur;
+};
+
+int main() {
+	Mahasiswa mhs;
+
+	cout << "Masukkan NIM : ";
+	cin.getline(mhs.nim, 12);
+	cout << "Masukkan Umur : ";
+	cin >> mhs.umur;
+	cin.ignore(1, '\n');
+	cout << "Masukkan Nama : ";
+	cin.getline(mhs.nama, 20);
+	cout << "Alamat : ";
+	cout << "\n\tMasukkan Desa : ";
+	cin.getline(mhs.alamat.desa, 20);
+	cout << "\tMasukkan Kota : ";
+	cin.getline(mhs.alamat.kota, 20);
+
+	cout << "\n\nNIM : " << mhs.nim;
+	cout << "\nUmur : " << mhs.umur;
+	cout << "\nnama : " << mhs.nama;
+	cout << "\nAlamat : ";
+	cout << "\n\tDesa : " << mhs.alamat.desa;
+	cout << "\n\tKota : " << mhs.alamat.kota;
+}
